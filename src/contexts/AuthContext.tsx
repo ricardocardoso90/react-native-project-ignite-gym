a05ccfbd3b934ed4d66ff1b796b7eecf182f526d
ricardocardoso90 <ricardomanolo90@gmail.com>
2024-08-22 15:49:21 -0300

    update commit

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -27,12 +27,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setUser(userData);
   };
 
-  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
+  async function storageUserAndTokenSave(userData: UserDTO, token: string, refresh_token: string) {
     try {
       setIsLoadingStogareUserData(true);
 
       await storageUserSave(userData);
-      await storageAuthTokenSave(token);
+      await storageAuthTokenSave({ token, refresh_token });
     } catch (error) {
       throw error;
     } finally {
@@ -44,8 +44,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post('/sessions', { email, password });
 
-      if (data.user && data.token) {
-        await storageUserAndTokenSave(data.user, data.token);
+      if (data.user && data.token && data.refresh_token) {
+        await storageUserAndTokenSave(data.user, data.token, data.refresh_token);
         userAndTokenUpdate(data.user, data.token);
       };
 
@@ -83,7 +83,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingStogareUserData(true);
 
       const userLogged = await storageUserGet();
-      const token = await storageAuthTokenGet();
+      const { token } = await storageAuthTokenGet();
 
       if (token && userLogged) {
         userAndTokenUpdate(userLogged, token);
@@ -97,7 +97,15 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   useEffect(() => {
     storageUserData();
-  }, [user]);
+  }, []);
+
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut);
+
+    return () => {
+      subscribe();
+    };
+  }, [signOut]);
 
   return (
     <AuthContext.Provider value={
--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -7,6 +7,7 @@ import { ScreenHeader } from "@screens/ScreenHeader";
 import { HistoryCard } from "@components/HistoryCard";
 import { useFocusEffect } from "@react-navigation/native";
 import { HistoryByDayDTO } from "@dtos/HistoryByDayDTO";
+import { Loading } from "@components/Loading";
 
 // const DATA = [
 //   {
@@ -55,29 +56,34 @@ export function History() {
         title="Histórico de Exercícios"
       />
 
-      <SectionList
-        sections={exercises}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <HistoryCard
-            data={item}
+      {isLoading
+        ? <Loading />
+        : (
+          <SectionList
+            sections={exercises}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <HistoryCard
+                data={item}
+              />
+            )}
+            renderSectionHeader={({ section }) => (
+              <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+                {section.title}
+              </Heading>
+            )}
+            px={8}
+            contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: "center" }}
+            ListEmptyComponent={() => (
+              <Text color="gray.100" textAlign="center">
+                Não há exercícios registrados ainda. {"\n"}
+                Vamos fazer exercícios hoje?
+              </Text>
+            )}
+            showsVerticalScrollIndicator={false}
           />
-        )}
-        renderSectionHeader={({ section }) => (
-          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
-            {section.title}
-          </Heading>
-        )}
-        px={8}
-        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: "center" }}
-        ListEmptyComponent={() => (
-          <Text color="gray.100" textAlign="center">
-            Não há exercícios registrados ainda. {"\n"}
-            Vamos fazer exercícios hoje?
-          </Text>
-        )}
-        showsVerticalScrollIndicator={false}
-      />
+        )
+      }
     </VStack>
   )
 };
